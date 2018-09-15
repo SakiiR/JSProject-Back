0d33ac0c2be6235624eb1d671ebba6057c75ace6
Pierrick MARTELLIERE <p.martelliere@gmail.com>
2018-09-15 14:59:01 +0200

    Enlèvement des propriétés inutiles sur les entités

--- room/Room.js
+++ room/Room.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var RoomSchema = new mongoose.Schema({
     id_creator: {type: Number, required: true},
-    status: {type: String, enum: ['pending', 'active', 'deleted'], default: 'pending'},
     name: {type: String, required: true},
     description: {type: String, required: true}
   }, 
