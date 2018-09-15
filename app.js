cb2397ba687ec428c51991d6d4258b538a67e0a9
Pierrick MARTELLIERE <p.martelliere@gmail.com>
2018-09-15 15:27:33 +0200

    .

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ io.on('connection', function(socket) {
 			// send client to room 1
 			socket.join(room);
 			// echo to client they've connected
-			socket.emit('updatechat', 'SERVER', 'you have connected to ' + room);
+			socket.emit('updateChat', 'SERVER', 'you have connected to ' + room);
 			// echo to room 1 that a person has connected to their room
-			socket.broadcast.to(room).emit('updatechat', 'SERVER', username + ' has connected to this room');
-			socket.emit('updaterooms', rooms, room);
+			socket.broadcast.to(room).emit('updateChat', 'SERVER', username + ' has connected to this room');
+			socket.emit('updateRooms', rooms, room);
 		}
 		else
 			socket.emit('error', 'Room inexistante');
@@ -59,7 +59,7 @@ io.on('connection', function(socket) {
 	        if (err) return res.status(500).send("There was a problem adding the information to the database.")
 	        else {
 	        	res.status(200).send(message);
-	        	io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+	        	io.sockets.in(socket.room).emit('updateChat', socket.username, data);
 	        }
 	    });
 	});
@@ -69,13 +69,13 @@ io.on('connection', function(socket) {
 		socket.leave(socket.room);
 		// join new room, received as function parameter
 		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+		socket.emit('updateChat', 'SERVER', 'you have connected to '+ newroom);
 		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+		socket.broadcast.to(socket.room).emit('updateChat', 'SERVER', socket.username+' has left this room');
 		// update socket session room title
 		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-		socket.emit('updaterooms', rooms, newroom);
+		socket.broadcast.to(newroom).emit('updateChat', 'SERVER', socket.username+' has joined this room');
+		socket.emit('updateRooms', rooms, newroom);
 	});
 
 	// when the user disconnects.. perform this
@@ -83,9 +83,9 @@ io.on('connection', function(socket) {
 		// remove the username from global usernames list
 		delete usernames[socket.username];
 		// update list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames);
+		io.sockets.emit('updateUsers', usernames);
 		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.broadcast.emit('updateChat', 'SERVER', socket.username + ' has disconnected');
 		socket.leave(socket.room);
 		console.log('weshout');
 	});
@@ -95,10 +95,16 @@ http.listen(API_PORT, function() {
 	console.log(`#BalanceTonPort: *:${API_PORT}`);
 });
 
+var AuthController = require(__root + 'auth/AuthController');
+app.use('/api/auth', AuthController);
+
 var UserController = require(__root + 'user/UserController');
 app.use('/api/users', UserController);
 
-var AuthController = require(__root + 'auth/AuthController');
-app.use('/api/auth', AuthController);
+var RoomController = require(__root + 'room/RoomController');
+app.use('/api/rooms', AuthController);
+
+var MessageController = require(__root + 'message/MessageController');
+app.use('/api/messages', AuthController);
 
 module.exports = app;
