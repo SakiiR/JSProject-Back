ff8bf98c09a8376816bd964c8b2535e347e983b2
Pierrick MARTELLIERE <p.martelliere@gmail.com>
2018-09-17 13:14:03 +0200

    .

--- message/Message.js
+++ message/Message.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var MessageSchema = new mongoose.Schema({
     id_sender: {type: Number, required: true},
     id_room: {type: Number, required: true},
-    status: {type: String, enum: ['pending', 'active', 'deleted'], default: 'pending'},
     content: {type: String, required: true}
   }, 
   {timestamps: true}
--- user/User.js
+++ user/User.js
@@ -2,9 +2,7 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    status: {type: String, enum: ['pending', 'active', 'deleted'], default: 'pending', required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true, unique: true}
+    password: {type: String, required: true}
   }, 
   {timestamps: true} // Pour avoir les dates de création et de modification automatiquement gérés par mongoose
 );
