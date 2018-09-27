fd0927a10d6a14ae36bdfe15c0a0c0790d3c127c
SakiiR SakiiR <sakiirlessong@gmail.com>
2018-09-27 01:37:49 -0700

    Fixed things, added auth middleware

--- src/routes/RouteIndex.js
+++ src/routes/RouteIndex.js
@@ -1,20 +1,24 @@
-import Route from './Route';
-import packageJson from '../../package.json';
+import Route from "./Route";
+import packageJson from "../../package.json";
+import authMiddleware from "../middlewares/Auth";
 
 @Route.Route({
-  routeBase: '',
+  routeBase: "/",
+  middlewares: [authMiddleware]
 })
-export default class RouteIndex extends Route {
+class RouteIndex extends Route {
   constructor(params) {
     super({ ...params });
   }
 
   // http://localhost:3000/
-  @Route.Get({ path: '/' })
+  @Route.Get({ path: "/" })
   index(ctx) {
     this.sendOk(ctx, {
       name: packageJson.name,
-      version: packageJson.version,
+      version: packageJson.version
     });
   }
 }
+
+export default RouteIndex;
--- src/routes/RouteRoom.js
+++ src/routes/RouteRoom.js
@@ -1,101 +1,109 @@
-import { Types } from 'koa-smart';
-import jwt from "jsonwebtoken";
-import Route from './Route';
-import User from '../models/Room';
-import Token from '../models/Token';
-import Message from '../models/Message';
-import { hashPassword, generateSalt, compareHash } from '../utils/hash';
+import { Types } from "koa-smart";
+import Route from "./Route";
+import Room from "../models/Room";
+import Message from "../models/Message";
+import { hashPassword, generateSalt } from "../utils/hash";
+import authMiddleware from "../middlewares/Auth";
 
-export default class RouteAuth extends Route {
-	constructor(params) {
-		super({ ...params });
-	}
+@Route.Route({
+  middlewares: [authMiddleware]
+})
+class RouteRoom extends Route {
+  constructor(params) {
+    super({ ...params });
+  }
 
-	async isAuthorized(ctx) {
-	    token = Token.findOne({value: ctx.headers.authorization});
-	    if (token === null)
-	    	return false;
-	    return true;
+  @Route.Get({
+    path: "/rooms/"
+  })
+  async list(ctx) {
+    let response = null;
+    const error = "No room found";
+    try {
+      const rooms = await Room.find({});
+      if (rooms === null) throw error;
+      response = {
+        rooms: rooms
+      };
+    } catch (err) {
+      return this.send(ctx, 404, err, null);
     }
+    this.sendOk(ctx, response, "Successfully read rooms");
+  }
 
-	@Route.Get({
-		accesses: [isAuthorized],
-		path: '/rooms/'
-	})
-	async list(ctx) {
-		let response = null;
-		const error = 'No room found';
-		try {
-			const rooms = await Room.find({});
-			if (rooms === null) throw error;
-			response = {
-				rooms: rooms
-			};
-		} catch (err) {
-			return this.send(ctx, 404, err, null);
-		}
-		this.sendOk(ctx, response, "Successfully read rooms");
-	}
+  @Route.Get({
+    path: "/rooms/:id/messages"
+  })
+  async get(ctx) {
+    let response = null;
+    const error = "No message found";
+    try {
+      const messages = await Message.find({ room: ctx.params.id });
+      if (messages === null) throw error;
+      response = {
+        messages: messages
+      };
+    } catch (err) {
+      return this.send(ctx, 404, err, null);
+    }
+    this.sendOk(
+      ctx,
+      response,
+      "Successfully read messages from room " + ctx.params.id
+    );
+  }
 
-	@Route.Get({
-		accesses: [isAuthorized],
-		path: '/rooms/:id/messages'
-	})
-	async list(ctx) {
-		let response = null;
-		const error = 'No message found';
-		try {
-			const messages = await Message.find({ room: ctx.params.id });
-			if (messages === null) throw error;
-			response = {
-				messages: messages
-			};
-		} catch (err) {
-			return this.send(ctx, 404, err, null);
-		}
-		this.sendOk(ctx, response, "Successfully read messages from room " + ctx.params.id);
-	}
+  @Route.Post({
+    path: "/rooms/",
+    bodyType: Types.object().keys({
+      creator: Types.number()
+        .integer()
+        .required(),
+      name: Types.string().required(),
+      description: Types.string().required()
+    })
+  })
+  async create(ctx) {
+    try {
+      const body = this.body(ctx);
+      const room = await Room.findOne({ name: body.name });
+      if (room !== null) throw "This room already exists !";
+      const salt = "";
+      const password = "";
+      if (body.private === true) {
+        const salt = generateSalt();
+        const password = await hashPassword(body.password + salt);
+      }
+      const result = await Room.create({
+        creator: body.creator,
+        name: body.name,
+        description: body.description,
+        private: body.private,
+        password: password,
+        salt: salt
+      });
+      if (result === null) return this.send(ctx, 403, "Failed to create room");
+    } catch (err) {
+      return this.send(ctx, 401, err, null);
+    }
+    this.sendOk(ctx, null, "Room created");
+  }
 
-	@Route.Post({
-		accesses: [isAuthorized],
-		path: '/rooms/',
-		bodyType: Types.object().keys({
-			creator: Types.number().integer().required(),
-			name: Types.string().required(),
-			description: Types.string().required()
-		})
-	})
-	async create(ctx) {
-		try {
-			const body = this.body(ctx);
-			const room = await Room.findOne({name: body.name});
-			if (room !== null)
-				throw "This room already exists !";
-			const salt = "";
-			const password = "";
-			if (body.private === true) {
-				const salt = generateSalt();
-				const password = await hashPassword(body.password + salt);
-			}
-			const result = await Room.create({ creator: body.creator, name: body.name, description: body.description, private: body.private, password: password, salt: salt});
-			console.log("Result: ", result);
-		} catch (err) {
-			return this.send(ctx, 401, err, null);
-		}
-		this.sendOk(ctx, null, "Room created");
-	}
-
-	@Route.Delete({
-		accesses: [isAuthorized],
-		path: '/rooms/:id'
-	})
-	async read(ctx) {
-		const error = 'This room doesn\'t exist';
-		try {
-			const rooms = await Room.findByIdAndRemove({id: ctx.params.id});
-		} catch (err) {
-			return this.send(ctx, 404, err, null);
-		}
-		this.sendOk(ctx, response, "Successfully deleted room");
-	}
+  @Route.Delete({
+    path: "/rooms/:id"
+  })
+  async delete(ctx) {
+    let response = {};
+    try {
+      const rooms = await Room.findByIdAndRemove({ id: ctx.params.id });
+      if (rooms === null)
+        return this.send(ctx, 403, "Failed to retrieve rooms to delete");
+      response["rooms"] = rooms;
+    } catch (err) {
+      return this.send(ctx, 404, err, null);
+    }
+    this.sendOk(ctx, response, "Successfully deleted room");
+  }
 }
+
+export default RouteRoom;
